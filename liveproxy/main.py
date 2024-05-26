19495540bbc8023cbcc92319761e0972ca7c316b
Alberto <alberto.rinaudo+dino@gmail.com>
2024-05-26 15:06:56 +0100

    add timestamp to logs
    Signed-off-by: Alberto <alberto.rinaudo+dino@gmail.com>

--- liveproxy/main.py
+++ liveproxy/main.py
@@ -22,7 +22,8 @@ def main():
     logging.basicConfig(
         stream=sys.stdout,
         level=args.loglevel,
-        format="[%(name)s][%(levelname)s] %(message)s",
+        format="%(asctime)s [%(name)s] [%(levelname)s] %(message)s",
+        datefmt='%Y-%m-%d %H:%M:%S'
     )
 
     if hasattr(os, "getuid"):
